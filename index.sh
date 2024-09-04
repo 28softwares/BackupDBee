e4b5d9fa425e89f3b1e8c5f34406f7265d2bed93
Anupam Bista <anupambista3@gmail.com>
2024-09-04 14:26:16 +0545

    uncommented installation check

--- index.sh
+++ index.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# if ! command -v zip &> /dev/null
-# then
-#     echo "zip command not found"
-#     exit 1
-# fi
+if ! command -v zip &> /dev/null
+then
+    echo "zip command not found"
+    exit 1
+fi
 
 
-# if ! command -v pg_dump &> /dev/null
-# then 
-#     echo "pg_dump command not found!"
-#     exit 1
-# fi
+if ! command -v pg_dump &> /dev/null
+then 
+    echo "pg_dump command not found!"
+    exit 1
+fi
 
 
-# if ! command -v mysqldump &> /dev/null
-# then 
-#     echo "mysqldump command not found!"
-#     exit 1
-# fi
+if ! command -v mysqldump &> /dev/null
+then 
+    echo "mysqldump command not found!"
+    exit 1
+fi
 
-# if ! command -v pnpm &> /dev/null
-# then 
-#     echo "pnpm command not found!"
-#     exit 1
-# fi
+if ! command -v pnpm &> /dev/null
+then 
+    echo "pnpm command not found!"
+    exit 1
+fi
 
-# if ! command -v node &> /dev/null
-# then 
-#     echo "node command not found!"
-#     exit 1
-# fi
+if ! command -v node &> /dev/null
+then 
+    echo "node command not found!"
+    exit 1
+fi
 
 
 # Function to prompt user input with a default value
