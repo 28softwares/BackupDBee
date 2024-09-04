c11b07b8f1deb4b0390c3d88021875b781bb2700
Anupam Bista <anupambista3@gmail.com>
2024-09-04 22:29:49 +0545

    setup and .env file creation can be done using cli

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 import process from "process";
+import fs from "fs";
 import { execSync } from "child_process";
 import chalk from "chalk";
 import { Command } from "commander";
-import { input } from "@inquirer/prompts";
+import { input, select } from "@inquirer/prompts";
 
 const program = new Command();
 program.version("1.0.0").description("AutoBackup DB CLI");
@@ -15,7 +16,7 @@ program
   .description(
     "Check required commands, create .env file and install dependencies"
   )
-  .action(() => {
+  .action(async () => {
     const commands = ["zip", "pg_dump", "mysqldump", "pnpm", "node"];
 
     commands.forEach((command) => {
@@ -27,6 +28,147 @@ program
         process.exit(1);
       }
     });
+
+    let backupOption = "";
+    let mailUser = "";
+    let mailPass = "";
+    let bucketName = "";
+    let accessKey = "";
+    let secretKey = "";
+    let region = "";
+    let backupNotification = "";
+    let slackWebhook = "";
+    let discordWebhook = "";
+    let postgresHost = "";
+    let postgresDbName = "";
+    let postgresDbUser = "";
+    let postgresDbPassword = "";
+    let mysqlHost = "";
+    let mysqlDbName = "";
+    let mysqlDbUser = "";
+    let mysqlDbPassword = "";
+
+    backupOption = await select({
+      message: "Please choose a backup option:",
+      choices: [
+        {
+          name: "Gmail",
+          value: "GMAIL",
+        },
+        {
+          name: "S3 Bucket",
+          value: "S3_BUCKET",
+        },
+      ],
+    });
+    if (backupOption === "GMAIL") {
+      mailUser = await input({ message: "Enter your email:" });
+      mailPass = await input({ message: "Enter your password:" });
+    } else if (backupOption === "S3_BUCKET") {
+      bucketName = await input({ message: "Enter your aws bucket name:" });
+      accessKey = await input({
+        message: "Enter your aws access key id:",
+      });
+      secretKey = await input({
+        message: "Enter your aws secret access key:",
+      });
+      region = await input({ message: "Enter your aws region:" });
+    } else {
+      console.log(chalk.red("Invalid option!"));
+      process.exit(1);
+    }
+
+    backupNotification = await select({
+      message: "Please choose a backup notification option:",
+      choices: [
+        {
+          name: "Slack",
+          value: "SLACK",
+        },
+        {
+          name: "Discord",
+          value: "DISCORD",
+        },
+        {
+          name: "Both",
+          value: "BOTH",
+        },
+      ],
+    });
+
+    if (backupNotification === "SLACK") {
+      slackWebhook = await input({ message: "Enter slack webhook url:" });
+    } else if (backupNotification === "DISCORD") {
+      discordWebhook = await input({ message: "Enter discord webhook url:" });
+    } else if (backupNotification === "BOTH") {
+      slackWebhook = await input({ message: "Enter slack webhook url:" });
+      discordWebhook = await input({ message: "Enter discord webhook url:" });
+    } else {
+      console.log(chalk.red("Invalid option!"));
+      process.exit(1);
+    }
+
+    postgresHost = await input({
+      message: "Enter your postgres host:",
+    });
+    postgresDbName = await input({
+      message: "Enter your postgres database name:",
+    });
+    postgresDbUser = await input({
+      message: "Enter your postgres database user:",
+    });
+    postgresDbPassword = await input({
+      message: "Enter your postgres database password:",
+    });
+    mysqlHost = await input({ message: "Enter your mysql host:" });
+    mysqlDbName = await input({
+      message: "Enter your mysql database name:",
+    });
+    mysqlDbUser = await input({
+      message: "Enter your mysql database user:",
+    });
+    mysqlDbPassword = await input({
+      message: "Enter your mysql database password:",
+    });
+
+    let envVariables = `#MAIL
+MAIL_USER=${mailUser}
+MAIL_PASSWORD=${mailPass}
+
+#BACKUP_CONFIG
+BACKUP_DEST=${backupOption}
+BACKUP_NOTIFICATION=${backupNotification}
+DISCORD_WEBHOOK_URL=${discordWebhook}
+SLACK_WEBHOOK_URL=${slackWebhook}
+
+# POSTGRES DATABASE CONFIGURATION
+POSTGRES_DB_HOST=${postgresHost}
+POSTGRES_DB_NAME=${postgresDbName}
+POSTGRES_DB_USER=${postgresDbUser}
+POSTGRES_DB_PASSWORD=${postgresDbPassword}
+
+# MYSQL DATABASE CONFIGURATION
+MYSQL_HOST=${mysqlHost}
+MYSQL_DB_NAME=${mysqlDbName}
+MYSQL_DB_USER=${mysqlDbUser}
+MYSQL_DB_PASSWORD=${mysqlDbPassword}
+
+# AWS S3 CONFIGURATION
+AWS_S3_BUCKET_NAME=${bucketName}
+AWS_ACCESS_KEY_ID=${accessKey}
+AWS_SECRET_ACCESS_KEY=${secretKey}
+AWS_REGION=${region}`;
+
+    try {
+      fs.writeFileSync(".env", envVariables);
+      console.log(chalk.green("Environment variables written successfully!"));
+    } catch (error) {
+      console.log(chalk.red("Failed to write environment variables!"));
+      console.log(chalk.red(error.message));
+      process.exit(1);
+    }
+
+    execSync("pnpm install && pnpm start");
   });
 
 program.parse(process.argv);
