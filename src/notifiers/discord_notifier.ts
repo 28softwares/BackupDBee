28fa7a735bdbbbe13de12b71ca3f8a79178c2d38
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-09-13 07:56:06 +0545

    Refactor: Remove console.log statements and update DiscordNotifier message property

--- src/notifiers/discord_notifier.ts
+++ src/notifiers/discord_notifier.ts
@@ -29,7 +29,7 @@ export class DiscordNotifier implements Notifier {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          text: this.message,
+          content: this.message,
         }),
       });
 
